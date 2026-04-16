60d2a2c350deead5126d1da45d98911c830d8828
kmy9317 <kimmin9632@gmail.com>
2026-04-16 17:57:04 +0900

    Initial Unreal project setup

---
+++ Source/AstralBreak/AstralBreak.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AstralBreak.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AstralBreak, "AstralBreak" );
